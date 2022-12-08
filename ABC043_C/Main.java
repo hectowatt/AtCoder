d0c342bd9a8750977f73c429d60a062fe4d1b274
hectowatt <0903tamaki@gmail.com>
2022-12-08 13:39:02 +0900

    ABC-043-c 平均との差の２乗の和でコストの総数が求められる

---
+++ ABC043_C/Main.java
@@ -0,0 +1,23 @@
+package ABC043_C;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        double[] num = new double[N];
+        double sum = 0;
+        for(int i=0;i<N; i++){
+            num[i] = sc.nextDouble();
+            sum += num[i];
+        }
+        double avr=Math.round(sum/N);
+        double ans=0;
+        for(int i=0;i<N;i++){
+            ans+=(num[i]-avr)*(num[i]-avr);
+        }
+
+        System.out.print((int)ans);
+
+    }
+}
